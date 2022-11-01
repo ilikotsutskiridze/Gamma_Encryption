6985e5620b789f0564dd37a793d83e3b5ac45ea2
ilikotsutskiridze <ilacuckiridze@gmail.com>
2022-11-01 21:25:49 +0300

    Сделал чистку кода

--- modulDZ/modulDZ.cpp
+++ modulDZ/modulDZ.cpp
@@ -1,18 +1,17 @@
-﻿/*
-Option 22
-
-4 byte
-Shift by 7
-*/
-
+﻿// iliko tsutskiridze [Option 22 (4 byte ___ Shift by 7)]
 
 #include <iostream>
 #include <clocale> //для setlocal>
 #include <string>
 #include <vector>
 
+using std::cin;
+using std::cout;
+using std::vector;
+using std::string;
 
-void shifr1(int key, int sdiv, std::string text)
+
+void shifr1(int key, int sdiv, string text)
 {
     // Устанавливаем начальное значение генератора псевдослучайных чисел, равное ключу
     srand(key);
@@ -21,9 +20,6 @@ void shifr1(int key, int sdiv, std::string text)
     int sim1, sim2, sim3, sim4; //первые четыре символа
     int shifr1, shifr2, shifr3, shifr4;//зашифрованные 4 символа
     int len; // Длина текста
-
-   
-
 
     len = text.length();
 
@@ -35,9 +31,8 @@ void shifr1(int key, int sdiv, std::string text)
         len++;
     };
 
-
-    std::vector<int> vmass0(len, unsigned char("="));//создаем буфферный массив
-    std::vector<int> vmass1(len, unsigned char("="));
+    vector<int> inmass0(len, unsigned char("="));//создаем буфферный массив
+    vector<int> inmass1(len, unsigned char("="));
 
     for (int i = 0; i < len; i = i + 4)
     {
@@ -49,50 +44,44 @@ void shifr1(int key, int sdiv, std::string text)
         sim3 = gam / 256;
         sim4 = gam % 256;
 
-
         //выполним операцию xor для первых 4 элементов
         shifr1 = unsigned char(sim1 ^ (text[i]));
         shifr2 = unsigned char(sim2 ^ (text[i + 1]));
         shifr3 = unsigned char(sim3 ^ (text[i + 2]));
         shifr4 = unsigned char(sim4 ^ (text[i + 3]));
         //запишем зашифрованные элементы в буферный массив
-        vmass0[i] = shifr1;
-        vmass0[i + 1] = shifr2;
-        vmass0[i + 2] = shifr3;
-        vmass0[i + 3] = shifr4;
-
-
+        inmass0[i] = shifr1;
+        inmass0[i + 1] = shifr2;
+        inmass0[i + 2] = shifr3;
+        inmass0[i + 3] = shifr4;
     }
 
     for (int i = 0; i < len; ++i) //сдвиг вправо на 7
-        vmass1[(i + sdiv) % len] = vmass0[i];
+        inmass1[(i + sdiv) % len] = inmass0[i];
 
     // Если последний символ равен значению заглушки, удаляем его
-    while (vmass0[len - 1] == int("="))
+    while (inmass0[len - 1] == int("="))
     {
-        vmass0.erase(vmass0.begin() + len - 1);
+        inmass0.erase(inmass0.begin() + len - 1);
         len--;
     }
 
-
-
     // Выводим в виде текста и в виде кода
 
-    std::cout << "\n\nЗашифрованный текст в символьном виде:\n";
+    cout << "\n\nЗашифрованный текст в символьном виде:\n";
 
     for (int i = 0; i < len; i++)
-        std::cout << unsigned char(vmass1[i]);
+        cout << unsigned char(inmass1[i]);
 
-    std::cout << "\n\nЗашифрованный текст в виде десятичного кода:\n";
+    cout << "\n\nЗашифрованный текст в виде десятичного кода:\n";
 
     for (int i = 0; i < len; i++)
-        std::cout << vmass1[i] << " ";
+        cout << inmass1[i] << " ";
 
-    std::cout << "\n\n" << "Зашифровал;)\nОтлично, что будет дальше?\n\n";
+    cout << "\n\n" << "Зашифровал;)\nОтлично, что будет дальше?\n\n";
 }
 
-
-void shifr2(int key, int sdiv, std::string text)
+void shifr2(int key, int sdiv, string text)
 {
     // Устанавливаем начальное значение генератора псевдослучайных чисел, равное ключу
     srand(key);
@@ -101,9 +90,6 @@ void shifr2(int key, int sdiv, std::string text)
     int sim1, sim2, sim3, sim4; //первые четыре символа
     int shifr1, shifr2, shifr3, shifr4;//зашифрованные 4 символа
     int dlin; // Длина текста
-
-
-
 
     dlin = text.length();
 
@@ -115,9 +101,8 @@ void shifr2(int key, int sdiv, std::string text)
         dlin++;
     };
 
-
-    std::vector<int> vmass0(dlin, unsigned char("="));//создаем буфферный массив
-    std::vector<int> vmass1(dlin, unsigned char("="));
+    vector<int> inmass0(dlin, unsigned char("="));//создаем буфферный массив
+    vector<int> inmass1(dlin, unsigned char("="));
 
     for (int i = 0; i < dlin; i = i + 4)
     {
@@ -129,58 +114,45 @@ void shifr2(int key, int sdiv, std::string text)
         sim3 = gam / 256;
         sim4 = gam % 256;
 
-
         //выполним операцию xor для первых 4 элементов
         shifr1 = unsigned char(sim1 ^ (text[i]));
         shifr2 = unsigned char(sim2 ^ (text[i + 1]));
         shifr3 = unsigned char(sim3 ^ (text[i + 2]));
         shifr4 = unsigned char(sim4 ^ (text[i + 3]));
         //запишем зашифрованные элементы в буферный массив
-        vmass0[i] = shifr1;
-        vmass0[i + 1] = shifr2;
-        vmass0[i + 2] = shifr3;
-        vmass0[i + 3] = shifr4;
-
-
+        inmass0[i] = shifr1;
+        inmass0[i + 1] = shifr2;
+        inmass0[i + 2] = shifr3;
+        inmass0[i + 3] = shifr4;
     }
 
     for (int i = 0; i < dlin; ++i)
-        vmass1[(dlin + i - sdiv) % dlin] = vmass0[i];
+        inmass1[(dlin + i - sdiv) % dlin] = inmass0[i];
 
     // Если последний символ равен значению заглушки, удаляем его
-    while (vmass0[dlin - 1] == int("="))
+    while (inmass0[dlin - 1] == int("="))
     {
-        vmass0.erase(vmass0.begin() + dlin - 1);
+        inmass0.erase(inmass0.begin() + dlin - 1);
         dlin--;
     }
 
-
-
     // Выводим в виде текста и в виде кода
 
-    std::cout << "\n\nЗашифрованный текст в символьном виде:\n";
+    cout << "\n\nЗашифрованный текст в символьном виде:\n";
 
     for (int i = 0; i < dlin; i++)
-        std::cout << unsigned char(vmass1[i]);
+        cout << unsigned char(inmass1[i]);
 
-
-    std::cout << "\n";
-
-    std::cout << "Зашифрованный текст в виде десятичного кода:\n";
+    cout << "\nЗашифрованный текст в виде десятичного кода:\n";
 
     for (int i = 0; i < dlin; i++)
-        std::cout << vmass1[i] << " ";
+        cout << inmass1[i] << " ";
 
-    std::cout << "\n\n" << "Зашифровал;)\n\n\n";
+    cout << "\n\nЗашифровал;)\n\n\n";
 }
 
-
-
-
-void rasshifr1(int key, int sdiv, std::string cod)
+void rasshifr1(int key, int sdiv, string cod)
 {
-    ///
-
     cod = cod + " ";
     // Устанавливаем начальное значение генератора псевдослучайных чисел, равное ключу
     srand(key);
@@ -189,19 +161,16 @@ void rasshifr1(int key, int sdiv, std::string cod)
     int rshifr1, rshifr2, rshifr3, rshifr4;//зашифрованные 4 символа
     int dlin, dlin1; // Длина текста
     
-    
-
     int a = 0;//создадим буферную переменную a
     dlin = cod.length();//узнаем строки
 
-    std::vector<int> vmass0;//создаем буфферный массив
+    vector<int> inmass0;//создаем буфферный массив
 
-    //687 467 23 23
     for (int i = 0; i < dlin; i++) //пока не закончится строка
     {
         if (cod[i] == ' ') //если нашли пробел...
         {
-            vmass0.push_back(a);//то запишем число а в вектор
+            inmass0.push_back(a);//то запишем число а в вектор
             a = 0;
         }
         else 
@@ -210,21 +179,20 @@ void rasshifr1(int key, int sdiv, std::string cod)
         }
     }
 
+    dlin1 = inmass0.size();
 
-    dlin1 = vmass0.size();
-
-    std::vector<int> vmass1(dlin1, 0);
+    vector<int> inmass1(dlin1, 0);
 
     //сдвигаем влево
     for (int i = 0; i < dlin1; ++i)
     {
         if(dlin1>sdiv)
-            vmass1[((dlin1 + i - sdiv) % dlin1)] = vmass0[i];
+            inmass1[((dlin1 + i - sdiv) % dlin1)] = inmass0[i];
         if (dlin1 < sdiv)
-            vmass1[(dlin1 + i - (sdiv - dlin+1)) % dlin1] = vmass0[i];
+            inmass1[(dlin1 + i - (sdiv - dlin+1)) % dlin1] = inmass0[i];
     }
 
-    std::vector<int> vmass3(dlin1, 0);
+    vector<int> inmass3(dlin1, 0);
 
     for (int i = 0; i < dlin1; i = i + 4)
     {
@@ -236,45 +204,35 @@ void rasshifr1(int key, int sdiv, std::string cod)
         sim3 = gam / 256;
         sim4 = gam % 256;
 
-
         //выполним операцию xor для первых 4 элементов
-        rshifr1 = unsigned char(sim1 ^ (vmass1[i]));
-        rshifr2 = unsigned char(sim2 ^ (vmass1[i + 1]));
-        rshifr3 = unsigned char(sim3 ^ (vmass1[i + 2]));
-        rshifr4 = unsigned char(sim4 ^ (vmass1[i + 3]));
+        rshifr1 = unsigned char(sim1 ^ (inmass1[i]));
+        rshifr2 = unsigned char(sim2 ^ (inmass1[i + 1]));
+        rshifr3 = unsigned char(sim3 ^ (inmass1[i + 2]));
+        rshifr4 = unsigned char(sim4 ^ (inmass1[i + 3]));
         //запишем зашифрованные элементы в буферный массив
-        vmass3[i] = rshifr1;
-        vmass3[i + 1] = rshifr2;
-        vmass3[i + 2] = rshifr3;
-        vmass3[i + 3] = rshifr4;
-
-
+        inmass3[i] = rshifr1;
+        inmass3[i + 1] = rshifr2;
+        inmass3[i + 2] = rshifr3;
+        inmass3[i + 3] = rshifr4;
     }
 
     // Если последний символ равен значению заглушки, удаляем его
-    while (vmass3[dlin1 - 1] == 61)
+    while (inmass3[dlin1 - 1] == 61)
     {
-        vmass3.erase(vmass3.begin() + dlin1 - 1);
+        inmass3.erase(inmass3.begin() + dlin1 - 1);
         dlin1--;
     }
 
-
-
-    std::cout << "\n\nРасшифрованный текст в символьном виде:\n";
+    cout << "\n\nРасшифрованный текст в символьном виде:\n";
 
     for (int i = 0; i < dlin1; i++)
-        std::cout << unsigned char(vmass3[i]);
+        cout << unsigned char(inmass3[i]);
 
-    ///
-
-    std::cout << "\n\nРасшифровал;)\n";
-    std::cout << "Отлично, что будет дальше?\n\n";
+    cout << "\n\nРасшифровал;)\nОтлично, что будет дальше?\n\n";
 }
 
-void rasshifr2(int key, int sdiv, std::string cod)
+void rasshifr2(int key, int sdiv, string cod)
 {
-    ///
-
     cod = cod + " ";
     // Устанавливаем начальное значение генератора псевдослучайных чисел, равное ключу
     srand(key);
@@ -283,20 +241,16 @@ void rasshifr2(int key, int sdiv, std::string cod)
     int rshifr1, rshifr2, rshifr3, rshifr4;//зашифрованные 4 символа
     int dlin, dlin1; // Длина текста
 
-
-
     int a = 0;//создадим буферную переменную a
     dlin = cod.length();//узнаем строки
 
-    std::vector<int> vmass0;//создаем буфферный массив
-
-    //654 657 89 98
+    vector<int> inmass0;//создаем буфферный массив
 
     for (int i = 0; i < dlin; i++) //пока не закончится строка
     {
         if (cod[i] == ' ') //если нашли пробел...
         {
-            vmass0.push_back(a);//то запишем число а в вектор
+            inmass0.push_back(a);//то запишем число а в вектор
             a = 0;
         }
         else
@@ -305,17 +259,15 @@ void rasshifr2(int key, int sdiv, std::string cod)
         }
     }
 
+    dlin1 = inmass0.size();
 
-    dlin1 = vmass0.size();
-
-    std::vector<int> vmass1(dlin1, 0);
+    vector<int> inmass1(dlin1, 0);
 
     //сдвигаем влево
     for (int i = 0; i < dlin1; ++i) 
-        vmass1[(i + sdiv) % dlin1] = vmass0[i];
+        inmass1[(i + sdiv) % dlin1] = inmass0[i];
 
-
-    std::vector<int> vmass3(dlin1, 0);
+    vector<int> inmass3(dlin1, 0);
 
     for (int i = 0; i < dlin1; i = i + 4)
     {
@@ -327,114 +279,99 @@ void rasshifr2(int key, int sdiv, std::string cod)
         sim3 = gam / 256;
         sim4 = gam % 256;
 
-
         //выполним операцию xor для первых 4 элементов
-        rshifr1 = unsigned char(sim1 ^ (vmass1[i]));
-        rshifr2 = unsigned char(sim2 ^ (vmass1[i + 1]));
-        rshifr3 = unsigned char(sim3 ^ (vmass1[i + 2]));
-        rshifr4 = unsigned char(sim4 ^ (vmass1[i + 3]));
+        rshifr1 = unsigned char(sim1 ^ (inmass1[i]));
+        rshifr2 = unsigned char(sim2 ^ (inmass1[i + 1]));
+        rshifr3 = unsigned char(sim3 ^ (inmass1[i + 2]));
+        rshifr4 = unsigned char(sim4 ^ (inmass1[i + 3]));
         //запишем зашифрованные элементы в буферный массив
-        vmass3[i] = rshifr1;
-        vmass3[i + 1] = rshifr2;
-        vmass3[i + 2] = rshifr3;
-        vmass3[i + 3] = rshifr4;
-
-
+        inmass3[i] = rshifr1;
+        inmass3[i + 1] = rshifr2;
+        inmass3[i + 2] = rshifr3;
+        inmass3[i + 3] = rshifr4;
     }
 
     // Если последний символ равен значению заглушки, удаляем его
-    while (vmass3[dlin1 - 1] == 61)
+    while (inmass3[dlin1 - 1] == 61)
     {
-        vmass3.erase(vmass3.begin() + dlin1 - 1);
+        inmass3.erase(inmass3.begin() + dlin1 - 1);
         dlin1--;
     }
 
-
-
-    std::cout << "\n\nРасшифрованный текст в символьном виде:\n";
+    cout << "\n\nРасшифрованный текст в символьном виде:\n";
 
     for (int i = 0; i < dlin1; i++)
-        std::cout << unsigned char(vmass3[i]);
+        cout << unsigned char(inmass3[i]);
 
-    ///
-
-    std::cout << "\n\nРасшифровал;)\n";
+    cout << "\n\nРасшифровал;)\n";
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-
     int a;
     bool exit;
     exit = true;
 
-
     while (exit)
     {
+        cout << "Выберете режим работы:\n";
+        cout << "Ведите 0, если хотите, чтобы программа работала в режиме шифровки\n";
+        cout << "Введите 1, если хотите, чтобы программа работала в режиме расшифровки\n";
+        cout << "Если хотите, чтобы программа завершила свою работу введите 2\n";
 
-        std::cout << "Выберете режим работы:\n";
-        std::cout << "Ведите 0, если хотите, чтобы программа работала в режиме шифровки\n";
-        std::cout << "Введите 1, если хотите, чтобы программа работала в режиме расшифровки\n";
-        std::cout << "Если хотите, чтобы программа завершила свою работу введите 2\n";
-
-
-        std::cin >> a;
+        cin >> a;
 
         if (a == 0)
         {
-           std::cout << "\nВведите направление сдвига: vlevo или vpravo\n";
+           cout << "\nВведите направление сдвига: vlevo или vpravo\n";
             int kluch, prover;
-            std::string text, a;
-            std::cin >> a;
+            string text, a;
+            cin >> a;
             a = "vpravo";
-             std::cout << "\nВведите ключ для шифровки:\n";
-            std::cin >> kluch;
-            std::cout << "\nВведите текст, который нужно зашифровать:\n";
+             cout << "\nВведите ключ для шифровки:\n";
+            cin >> kluch;
+            cout << "\nВведите текст, который нужно зашифровать:\n";
 
-            std::cin.ignore();
-            std::getline(std::cin, text);
+            cin.ignore();
+            std::getline(cin, text);
 
             if (a == "vpravo")
                 shifr1(kluch, 7, text);
             if (a == "vlevo")
                 shifr2(kluch, 7, text);
-
         }
 
         if (a == 1)
         {
             int kluch1, prover1;
-            std::string a1;
+            string a1;
 
-
-           std::cout << "\nВведите направление сдвига, который вы ввели во время шифровки: vlevo или vpravo\n";
-           std::cin >> a1;
+           cout << "\nВведите направление сдвига, который вы ввели во время шифровки: vlevo или vpravo\n";
+           cin >> a1;
             a1 = "vpravo";
-            std::cout << "Введите ключ для расшифровки:(он должен равняться ключу, использованному для зашифровки этой сторки)\n";
-            std::cin >> kluch1;
+            cout << "Введите ключ для расшифровки:(он должен равняться ключу, использованному для зашифровки этой сторки)\n";
+            cin >> kluch1;
 
-            std::cin.ignore();
-            std::cout << "Введите код:\n";
-            std::string cod;
-            std::getline(std::cin, cod);
+            cin.ignore();
+            cout << "Введите код:\n";
+            string cod;
+            std::getline(cin, cod);
 
             if (a1 == "vpravo")
                 rasshifr1(kluch1, 7, cod);
             if (a1 == "vlevo")
                 rasshifr2(kluch1, 7, cod);
-
         }
         if (a == 2)
         {
-            std::cout << "Работа программы завершена\n";
+            cout << "Работа программы завершена\n";
             exit = false;
         }
 
         if ((a != 1) && (a != 0) && (a != 2))
-            std::cout << "Команда не распознана. Следуйте указаниям по выбору режима работы:\n";
-
+            cout << "Команда не распознана. Следуйте указаниям по выбору режима работы:\n";
     }
 }
 
