c7a55db3b175661df6ca7852d52271c471ba372e
ilikotsutskiridze <ilacuckiridze@gmail.com>
2022-11-07 00:21:46 +0300

    Optimized encryption and decryption functions.

--- modulDZ/modulDZ.cpp
+++ modulDZ/modulDZ.cpp
@@ -41,7 +41,7 @@ void EncryptHelper()
 }
 
 
-void Encryption1(int key, int sdiv, string text)
+void Encryption(int key, int sdiv, string text, string route)
 {
     // Устанавливаем начальное значение генератора псевдослучайных чисел, равное ключу
     srand(key);
@@ -86,8 +86,13 @@ void Encryption1(int key, int sdiv, string text)
         inmass0[i + 3] = enc4;
     }
 
-    for (int i = 0; i < len; ++i) //сдвиг вправо на 7
-        inmass1[(i + sdiv) % len] = inmass0[i];
+    if (route == "right")
+        for (int i = 0; i < len; ++i) //сдвиг вправо на 7
+            inmass1[(i + sdiv) % len] = inmass0[i];
+    else if (route == "left")
+        for (int i = 0; i < len; ++i) //сдвиг влево на 7
+            inmass1[(len + i - sdiv) % len] = inmass0[i];
+
 
     // Если последний символ равен значению заглушки, удаляем его
     while (inmass0[len - 1] == int("="))
@@ -108,80 +113,10 @@ void Encryption1(int key, int sdiv, string text)
     for (int i = 0; i < len; i++)
         cout << inmass1[i] << " ";
 
-    cout << "\n\n" << "Зашифровал;)\nОтлично, что будет дальше?\n\n";
+    cout << "\n\n" << "Зашифровал!\nОтлично, что будет дальше?\n\n";
 }
 
-void Encryption2(int key, int sdiv, string text)
-{
-    // Устанавливаем начальное значение генератора псевдослучайных чисел, равное ключу
-    srand(key);
-
-    int gam; // Функция гамма для каждого блока
-    int sim1, sim2, sim3, sim4; //первые четыре символа
-    int enc1, enc2, enc3, enc4;//зашифрованные 4 символа
-    int dlin; // Длина текста
-
-    dlin = text.length();
-
-    // Если текст не делиться нацело на блоки, прибавляем к тексту стоп-символ "="
-
-    while (dlin % 4 != 0)
-    {
-        text += '=';
-        dlin++;
-    };
-
-    vector<int> inmass0(dlin, unsigned char("="));//создаем буфферный массив
-    vector<int> inmass1(dlin, unsigned char("="));
-
-    for (int i = 0; i < dlin; i = i + 4)
-    {
-        //создадим для первых 4 элементов гаммы
-        gam = rand();
-        sim1 = gam / 256;
-        sim2 = gam % 256;
-        gam = rand();
-        sim3 = gam / 256;
-        sim4 = gam % 256;
-
-        //выполним операцию xor для первых 4 элементов
-        enc1 = unsigned char(sim1 ^ (text[i]));
-        enc2 = unsigned char(sim2 ^ (text[i + 1]));
-        enc3 = unsigned char(sim3 ^ (text[i + 2]));
-        enc4 = unsigned char(sim4 ^ (text[i + 3]));
-        //запишем зашифрованные элементы в буферный массив
-        inmass0[i] = enc1;
-        inmass0[i + 1] = enc2;
-        inmass0[i + 2] = enc3;
-        inmass0[i + 3] = enc4;
-    }
-
-    for (int i = 0; i < dlin; ++i)
-        inmass1[(dlin + i - sdiv) % dlin] = inmass0[i];
-
-    // Если последний символ равен значению заглушки, удаляем его
-    while (inmass0[dlin - 1] == int("="))
-    {
-        inmass0.erase(inmass0.begin() + dlin - 1);
-        dlin--;
-    }
-
-    // Выводим в виде текста и в виде кода
-
-    cout << "\n\nЗашифрованный текст в символьном виде:\n";
-
-    for (int i = 0; i < dlin; i++)
-        cout << unsigned char(inmass1[i]);
-
-    cout << "\nЗашифрованный текст в виде десятичного кода:\n";
-
-    for (int i = 0; i < dlin; i++)
-        cout << inmass1[i] << " ";
-
-    cout << "\n\nЗашифровал;)\n\n\n";
-}
-
-void Decryption1(int key, int sdiv, string cod)
+void Decryption(int key, int sdiv, string cod, string route)
 {
     cod = cod + " ";
     // Устанавливаем начальное значение генератора псевдослучайных чисел, равное ключу
@@ -213,14 +148,17 @@ void Decryption1(int key, int sdiv, string cod)
 
     vector<int> inmass1(dlin1, 0);
 
-    //сдвигаем влево
-    for (int i = 0; i < dlin1; ++i)
-    {
-        if(dlin1>sdiv)
-            inmass1[((dlin1 + i - sdiv) % dlin1)] = inmass0[i];
-        if (dlin1 < sdiv)
-            inmass1[(dlin1 + i - (sdiv - dlin+1)) % dlin1] = inmass0[i];
-    }
+    if (route == "right")
+        for (int i = 0; i < dlin1; ++i)
+        {
+            if(dlin1 > sdiv)
+                inmass1[((dlin1 + i - sdiv) % dlin1)] = inmass0[i];
+            if (dlin1 < sdiv)
+                inmass1[(dlin1 + i - (sdiv - dlin+1)) % dlin1] = inmass0[i];
+        }
+    else if (route == "left")
+        for (int i = 0; i < dlin1; ++i)
+            inmass1[(i + sdiv) % dlin1] = inmass0[i];
 
     vector<int> inmass3(dlin1, 0);
 
@@ -258,82 +196,7 @@ void Decryption1(int key, int sdiv, string cod)
     for (int i = 0; i < dlin1; i++)
         cout << unsigned char(inmass3[i]);
 
-    cout << "\n\nРасшифровал;)\nОтлично, что будет дальше?\n\n";
-}
-
-void Decryption2(int key, int sdiv, string cod)
-{
-    cod = cod + " ";
-    // Устанавливаем начальное значение генератора псевдослучайных чисел, равное ключу
-    srand(key);
-    int gam; // Функция гамма для каждого блока
-    int sim1, sim2, sim3, sim4; //первые четыре символа
-    int dec1, dec2, dec3, dec4;//зашифрованные 4 символа
-    int dlin, dlin1; // Длина текста
-
-    int a = 0;//создадим буферную переменную a
-    dlin = cod.length();//узнаем строки
-
-    vector<int> inmass0;//создаем буфферный массив
-
-    for (int i = 0; i < dlin; i++) //пока не закончится строка
-    {
-        if (cod[i] == ' ') //если нашли пробел...
-        {
-            inmass0.push_back(a);//то запишем число а в вектор
-            a = 0;
-        }
-        else
-        {
-            a = a * 10 + (unsigned char(cod[i]) - 48);
-        }
-    }
-
-    dlin1 = inmass0.size();
-
-    vector<int> inmass1(dlin1, 0);
-
-    //сдвигаем влево
-    for (int i = 0; i < dlin1; ++i) 
-        inmass1[(i + sdiv) % dlin1] = inmass0[i];
-
-    vector<int> inmass3(dlin1, 0);
-
-    for (int i = 0; i < dlin1; i = i + 4)
-    {
-        //создадим для первых 4 элементов гаммы
-        gam = rand();
-        sim1 = gam / 256;
-        sim2 = gam % 256;
-        gam = rand();
-        sim3 = gam / 256;
-        sim4 = gam % 256;
-
-        //выполним операцию xor для первых 4 элементов
-        dec1 = unsigned char(sim1 ^ (inmass1[i]));
-        dec2 = unsigned char(sim2 ^ (inmass1[i + 1]));
-        dec3 = unsigned char(sim3 ^ (inmass1[i + 2]));
-        dec4 = unsigned char(sim4 ^ (inmass1[i + 3]));
-        //запишем зашифрованные элементы в буферный массив
-        inmass3[i] = dec1;
-        inmass3[i + 1] = dec2;
-        inmass3[i + 2] = dec3;
-        inmass3[i + 3] = dec4;
-    }
-
-    // Если последний символ равен значению заглушки, удаляем его
-    while (inmass3[dlin1 - 1] == 61)
-    {
-        inmass3.erase(inmass3.begin() + dlin1 - 1);
-        dlin1--;
-    }
-
-    cout << "\n\nРасшифрованный текст в символьном виде:\n";
-
-    for (int i = 0; i < dlin1; i++)
-        cout << unsigned char(inmass3[i]);
-
-    cout << "\n\nРасшифровал;)\n";
+    cout << "\n\nРасшифровал!\nОтлично, что будет дальше?\n\n";
 }
 
 int main()
@@ -369,10 +232,7 @@ int main()
             cin.ignore();
             std::getline(cin, text);
 
-            if (route == "right")
-                Encryption1(stoi(key), 7, text);
-            if (route == "left")
-                Encryption2(stoi(key), 7, text);
+            Encryption(stoi(key), 7, text, route);
         }
 
         if (mode == 1)
@@ -393,10 +253,7 @@ int main()
             string code;
             std::getline(cin, code);
 
-            if (route == "right")
-                Decryption1(stoi(key), 7, code);
-            if (route == "left")
-                Decryption2(stoi(key), 7, code);
+            Decryption(stoi(key), 7, code, route);
         }
         if (mode == 2)
         {
